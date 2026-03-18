4c9f9e000799fab2819070dceb477c6cb94f9f25
Casper van Laer <caspervlaer@outlook.com>
2026-03-18 22:34:54 +0100

    added steam achievement
    made it that once you score 500 or above it will activate the steam achievement win game

--- Minigin/ScoreObserver.cpp
+++ Minigin/ScoreObserver.cpp
@@ -4,6 +4,13 @@
 #include "ScoreComponent.h"
 #include "GameObject.h"
 
+#if USE_STEAMWORKS
+#pragma warning(push)
+#pragma warning(disable: 4996)
+#include <steam_api.h>
+#pragma warning(pop)
+#endif
+
 ScoreObserver::ScoreObserver(dae::GameObject* owner)
 	: Component(owner)
 {
@@ -23,6 +30,22 @@ void ScoreObserver::OnNotify(dae::GameObject& entity, dae::Event event)
 			{
 				textComponent->SetText("Score: " + std::to_string(score));
 			}
+
+			CheckWinCondition(score);
 		}
 	}
 }
+
+void ScoreObserver::CheckWinCondition(int score)
+{
+	if(score >= m_WinScore && !m_GameWon)
+	{
+		m_GameWon = true;
+		
+#if USE_STEAMWORKS
+		SteamUserStats()->SetAchievement("ACH_WIN_ONE_GAME");
+		SteamUserStats()->StoreStats();
+#endif
+	}
+}
+
